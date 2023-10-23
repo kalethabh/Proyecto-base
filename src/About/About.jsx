4733e7bf9b203bfd0403938bf8482a951bf3af61
Kaleth Alexander Benjumea Hernandez <113316840+kalethabh@users.noreply.github.com>
2023-10-23 14:00:22 +0000

    About

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -6,19 +6,20 @@ const developers = [
 ];
 
 return (
-    <div className="bg-white p-6 rounded-lg shadow-md flex justify-center max-h-full">
-        <div>
+        <div className="flex justify-center mt-4">
+    <div className="bg-white p-6 rounded-lg shadow-md  h-[100rem] w-[80em]">
 
       <h1 className="text-3xl font-bold mb-4">Acerca de nuestra organización sin fines de lucro</h1>
       <p className="text-gray-700 text-lg">
-        Somos una organización sin fines de lucro comprometida con la mejora de la comunidad y la causa que defendemos. Nuestra misión es...
+        Somos una organización sin fines de lucro comprometida con la mejora de la comunidad y la causa que defendemos. Nuestra misión es mejorar la calidad de vida de las personas y promover el bienestar en todas las áreas de la sociedad.
       </p>
       <p className="text-gray-700 text-lg mt-4">
-        Trabajamos incansablemente para lograr un impacto positivo en la vida de las personas a través de nuestros proyectos y programas. Creemos en la importancia de...
+        Trabajamos incansablemente para lograr un impacto positivo en la vida de las personas a través de nuestros proyectos y programas. Creemos en la importancia de la educación, la igualdad, la sostenibilidad y el apoyo a quienes más lo necesitan.
       </p>
       <p className="text-gray-700 text-lg mt-4">
-        Nuestro equipo de desarrolladores está dedicado a hacer posible nuestra misión. Conoce a algunos de nuestros talentosos desarrolladores:
+        Nuestra organización ha estado bastante activa, durante todo este tiempo hemos logrado impactar positivamente a miles de personas en la comunidad. Estamos comprometidos con la transparencia y la rendición de cuentas en todo lo que hacemos.
       </p>
+      <h1 className="mt-6 font-extrabold">DESARROLLADORES</h1>
       <ul className="list-disc list-inside mt-2">
         {developers.map((developer, index) => (
             <li key={index} className="text-gray-700 text-lg">{developer.name} - {developer.role}</li>
@@ -27,8 +28,8 @@ return (
       <p className="text-gray-700 text-lg mt-4">
         Tu apoyo y participación son fundamentales para lograr nuestros objetivos. ¡Únete a nosotros en nuestra misión y juntos haremos del mundo un lugar mejor!
       </p>
-            </div>
     </div>
+            </div>
   );
 }
 
